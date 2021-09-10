d32b0ec4e604547fa8aa5ffb0328356c8762a567
karthik29913 <88362651+karthik29913@users.noreply.github.com>
2021-09-10 21:01:43 +0530

    Add files via upload

---
+++ Parsing.java
@@ -0,0 +1,11 @@
+class Division
+ {
+   public static void main(String args[])
+    {
+        int i1=Integer.parseInt(args[0]);
+        double i2=Double.parseDouble(args[1]);
+        
+        System.out.println();
+        System.out.println();
+    }
+}
